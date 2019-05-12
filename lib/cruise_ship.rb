5c92bb652d97b9d4354431a1529141105b7e6c95
Haley DeKeyser <hcd09@live.com>
2019-05-12 17:52:04 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -6,10 +6,13 @@
 # suite_e: "Crumpet the Elf"
 # }
 
+winner = ""
+
 def select_winner(passengers)
   passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
       winner = name
-    end
   end
 end
+
+winner
