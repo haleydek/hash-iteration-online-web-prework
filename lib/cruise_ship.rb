5c7c2600dc195ea296e25517f8d38fbc623bd566
Haley DeKeyser <hcd09@live.com>
2019-05-12 17:56:13 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -12,8 +12,7 @@ def select_winner(passengers)
   passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
       winner = name
-    end
-  winner
+      winner
   end
 end
 
